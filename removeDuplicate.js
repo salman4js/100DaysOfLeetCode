1b2142e36980ce73ba731885b5895d5b8e0d58db
Salman4Js <92971152+salman4js@users.noreply.github.com>
2022-02-21 02:05:47 +0530

    Update removeDuplicate.js

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,9 +1,9 @@
 const remove = (arr) => {
     let index = 0;
     for(let i = 0; i < arr.length; i ++){
-        console.log(arr[i]);
+       
         if(arr[i] != arr[i+1]){
-            console.log("arr[i]  " + arr[i] + "  arr[i+1]  " + arr[i+1]);
+            
             arr[index] = arr[i];
             index++;
         }
@@ -11,4 +11,3 @@ const remove = (arr) => {
     
 }
 
-console.log(remove([1,1,2]))
